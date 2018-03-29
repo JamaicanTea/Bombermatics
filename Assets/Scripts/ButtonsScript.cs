9686f0bc88a2d3b6493ad8daa309ecdcf6bce47c
Acxell Leandro <33308802+JamaicanTea@users.noreply.github.com>
2018-03-29 15:38:31 +1100

    Functionality added for each scenes

---
+++ Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScript : MonoBehaviour {
+	public GameObject gameOver;
+	public GameObject resetButton;
+
+	// Use this for initialization
+	public void RightAnswer () 
+	{
+		SceneManager.LoadScene ("GameScene2");	
+	}
+
+	public void RightAnswer2 () 
+	{
+		SceneManager.LoadScene ("GameScene3");	
+	}
+	public void RightAnswer3 () 
+	{
+		SceneManager.LoadScene ("GameScene4");	
+	}
+	public void RightAnswer4 () 
+	{
+		SceneManager.LoadScene ("GameScene2");	
+	}
+	// Update is called once per frame
+	public void GameOver () 
+	{
+		Time.timeScale = 0;
+		gameOver.gameObject.SetActive (true);
+		resetButton.gameObject.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+	public Text timer;
+	public float time = 30.0f;
+	public GameObject gameOver;
+	public GameObject resetButton;
+	// Use this for initialization
+	void Awake () 
+	{
+		Time.timeScale = 1;
+		timer.text = time.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time -= Time.deltaTime;
+		DisplayTime ();
+		if (time < 0) 
+		{
+			GameOver ();
+		}
+	}
+
+	void DisplayTime ()
+	{
+		timer.text = "" + Mathf.Round (time);
+	}
+
+	void GameOver()
+	{
+		Time.timeScale = 0;
+		gameOver.gameObject.SetActive (true);
+		resetButton.gameObject.SetActive (true);
+	}
+}
